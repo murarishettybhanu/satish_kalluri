3433a9214de097f95ad92721615237bcd349b233
murarishettybhanu <murarishettybhanuteja@gmail.com>
2026-01-17 04:00:35 +0530

    Switch from gh-pages to GitHub Actions for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/satish_kalluri/',
+  base: '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
